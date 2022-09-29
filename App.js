c9d973d3e5b5edbca8f83a73cdd0c2aa9e37fb2b
Cosmin Carsote <cosmin.c@adoreme.com>
2022-09-29 23:39:19 +0300

    bag pla in vs code si git

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Bag pola in gittttttt si in sync vscode ca am 82475892 de conturi si d
+        aia nu mergea
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
