9481ddef32d11c7fac9f36fa819066f7ffc72f1e
Vlad Costin Andrei <vldcostin@gmail.com>
2022-09-30 21:39:50 +0300

    Demo 6 butoane, array de obiecte hardcodat cu cine ce rol are / cu cn voteaza, si CALCUL PUNCTAJ

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -1,51 +1,121 @@
 import React from 'react';
 
-import { Center, Text, Button, v3CompatibleTheme } from 'native-base';
-import { RefreshControl } from 'react-native';
+import { Center, Text, Button } from 'native-base';
 
 let players = [
   {
     name: 'Dragos',
     role: 'rat',
-    votes: 0, 
+    fakeid: 'Matei',
+    vote: 'Mario',
+    nrOfVotes: 0, 
     score: 0
   },
   {
     name: 'Cosmin',
     role: 'cat',
-    votes: 0, 
+    fakeid: 'Cosmin',
+    vote: 'Matei',
+    nrOfVotes: 0, 
     score: 0
   }, 
   {
     name: 'Mario',
     role: 'rat',
-    votes: 0, 
+    fakeid: 'Dragos',
+    vote: 'Cosmin',
+    nrOfVotes: 0, 
     score: 0
   },
   {
-  name: 'David',
-  role: 'cat',
-  votes: 0, 
-  score: 0
+    name: 'David',
+    role: 'cat',
+    fakeid: 'David',
+    vote: 'Mario',
+    nrOfVotes: 0, 
+    score: 0
   },
   {
     name: 'Costin',
     role: 'cat',
-    votes: 0, 
+    fakeid: 'Costin',
+    vote: 'Mario',
+    nrOfVotes: 0, 
+    score: 0
+  },
+  {
+    name: 'Matei',
+    role: 'rat',
+    fakeid: 'Mario',
+    vote: 'Dragos',
+    nrOfVotes: 0,
     score: 0
   }
 ];
 
-function voteFor(playerName){
-  let indexOfVoted = players.findIndex(player => player.name == playerName);
+function voteFor(playerName) {
+  let indexOfVoted = players.findIndex(player => player.fakeid == playerName);
   players[indexOfVoted].votes++;
   window.alert(players[indexOfVoted].votes);
 };
 
+function resetVotes() {
+  const nrOfPlayers = players.length;
+
+  for (let i = 0; i < nrOfPlayers; i++) {
+    players[i].votes = players[i].score = 0;
+  }
+}
+
+function roundUp10(num) {
+  return Math.ceil(num / 10) * 10;
+}
+
+function calculateScore() {
+  const nrOfPlayers = players.length;
+
+  for (let i = 0; i < nrOfPlayers; i++) {
+    let indexOfVoted = players.findIndex(player => player.fakeid == players[i].vote);
+
+    if (players[i].role === 'cat') { // real
+      if (players[indexOfVoted].role === 'rat') {
+        players[i].score += nrOfPlayers - 1 - players[indexOfVoted].nrOfVotes;
+      }
+      
+      if (players[i].score < 1) {
+          players[i].score = 1;
+      }
+      
+      players[i].score *= 10;
+      
+      if (players[i].score == 10 * (nrOfPlayers - 2)) {
+        players[i].score += 5;
+      }
+    } else { // fake
+      players[i].score += nrOfPlayers - 2 - players[i].nrOfVotes;
+      players[i].score *= 10;
+      
+      if (players[i].score == 10 * (nrOfPlayers - 2)){
+        players[i].score += 5;
+      }
+    }
+
+    players[i].score = roundUp10(players[i].score * 76 / nrOfPlayers);
+  }
+
+  window.alert("Scoreboard:\nDragos: " + players[0].score +
+                          "\nCosmin: " + players[1].score +
+                          "\nMario: " + players[2].score +
+                          "\nDavid: " + players[3].score + 
+                          "\nCostin: " + players[4].score + 
+                          "\nMatei: " + players[5].score);
+}
+
+
 export const ForgotPasswordPage = () => {
   return (
-    <Center w="100%" h="100%" bgColor="primary1.500">
-      <Text paddingBottom="5px">Forgot Password Page</Text>
+    <Center w="100%" h="100%" bgColor="coolGray.300">
+      <Text paddingBottom="5px">Vote out the rats!</Text>
       <Button
         w="20%" h="5%"
         marginBottom="4px"
@@ -100,20 +170,42 @@ export const ForgotPasswordPage = () => {
       >
         Costin
       </Button>
-      
+
+      <Button
+        w="20%" h="5%"
+        marginBottom="4px"
+        padding="1px"
+        onPress={() => {
+          voteFor('Matei');
+        }}
+      >
+        Matei
+      </Button>
+
       <Button
         w="20%" h="5%"
         marginBottom="4px"
         padding="1px"
         bgColor="amber.900"
         onPress={() => {
-          players.forEach(function (player){
-            player.votes = 0;
-          });
+          resetVotes();
         }}
       >
         Reset
       </Button>
+
+      <Button
+        w="20%" h="5%"
+        marginBottom="4px"
+        padding="1px"
+        bgColor="emerald.600"
+        onPress={() => {
+          calculateScore();
+        }}
+      >
+        Stop Vote!
+      </Button>
+      
     </Center>
   );
 };
