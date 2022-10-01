5a2e6fd70fe47e6fc40d8dcce9fff6a2fbd2fb4a
Cosmin Carsote <cosmin.c@adoreme.com>
2022-10-01 15:17:38 +0300

    pt iubirea mea absoluta si eterna dragos :)

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -1,62 +1,71 @@
-import React from 'react';
-import InGamePage from './InGame';
+import React, { useState } from 'react';
+
 import { Center, Button, Text } from 'native-base';
 
-var players = [
+import {
+  collection,
+  db,
+  getDocs,
+} from '../../../config/firebase/firebase-key-config';
+
+let players = [
   {
     name: 'Dragos', //index 0
     role: 'cat',
     no_of_votes: 0,
-    vote: 2, 
+    vote: 2,
     fakeid: null,
-    score: 0
+    score: 0,
   },
   {
     name: 'Cosmin', //index 1
     role: 'cat',
     no_of_votes: 0,
     vote: 2,
-    fakeid: null, 
-    score: 0
-  }, 
+    fakeid: null,
+    score: 0,
+  },
   {
     name: 'Mario', //index 2
     role: 'cat',
     no_of_votes: 0,
     vote: 0,
-    fakeid: null, 
-    score: 0
+    fakeid: null,
+    score: 0,
   },
   {
-  name: 'David', //index 3
-  role: 'cat',
-  no_of_votes: 0,
-  vote: 0,
-  fakeid: null, 
-  score: 0
+    name: 'David', //index 3
+    role: 'cat',
+    no_of_votes: 0,
+    vote: 0,
+    fakeid: null,
+    score: 0,
   },
   {
     name: 'Costin', //index 4
     role: 'cat',
     no_of_votes: 0,
-    vote: 3, 
+    vote: 3,
     fakeid: null,
-    score: 0
+    score: 0,
   },
   {
     name: 'Matei', //index 5
     role: 'cat',
     no_of_votes: 0,
     vote: -1,
-    fakeid: null, 
-    score: 0
-  }
+    fakeid: null,
+    score: 0,
+  },
 ];
 
 export const ForgotPasswordPage = ({ navigation }) => {
-  function set_roles() {
+  const [playersDB, setPlayersDB] = useState([]);
+
+  const setRoles = () => {
     let no_of_rats = players.length / 2;
     let cnt = 0;
+
     while (cnt < no_of_rats) {
       let val = Math.floor(Math.random() * 5) + 1;
       if (players[val].role === 'cat') {
@@ -64,38 +73,66 @@ export const ForgotPasswordPage = ({ navigation }) => {
         cnt++;
       }
     }
-    for(let i = 0; i < players.length; i++)
+
+    for (let i = 0; i < players.length; i++) {
       players[i].fakeid = players[i].name;
-    let res = players.filter( role => role.role === 'rat');
+    }
+
+    let res = players.filter((role) => role.role === 'rat');
     res[0].fakeid = res[no_of_rats - 1].name;
+
     for (let i = 1; i < no_of_rats; i++) {
       res[i].fakeid = res[i - 1].name;
     }
 
-    navigation.navigate('InGame')
+    navigation.navigate('InGame');
     //alert(JSON.stringify(res));
-    
+
     // reset roles
-    
-    
-  }
+  };
+
+  const getPlayers = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'games/abcd/players'));
+      let playersArray = [];
+
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, ' => ', doc.data());
+        playersArray.push(doc.data());
+      });
+
+      setPlayersDB(playersArray);
+    } catch (err) {
+      console.log('Error: ', err);
+    }
+  };
+
   return (
-    
     <Center w="100%" h="100%" bgColor="primary1.500">
       <Button
-        onPress={() => 
-          set_roles()
-          
-        }
+        onPress={() => {
+          setRoles();
+        }}
       >
         Apasa
       </Button>
-      
+
+      <Button
+        onPress={() => {
+          getPlayers();
+        }}
+      >
+        Nu apasa
+      </Button>
+
+      {playersDB &&
+        playersDB.map((player, index) => {
+          return <Text key={index}>{player.name}</Text>;
+        })}
     </Center>
   );
 };
 
-
-
 export default ForgotPasswordPage;
-export {players};
+export { players };
