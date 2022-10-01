cf3d4310daef1a5b4b4df2c3c1d9a10da9852a81
Vlad Costin Andrei <vldcostin@gmail.com>
2022-10-01 17:44:38 +0300

    Nu mai apar toate butoanele, scoreboard sortat, apar fakerii la final de runda, se reseteaza voturile

--- src/pages/auth/ForgotPassword.jsx
+++ src/pages/auth/ForgotPassword.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { Center, Text, Button } from 'native-base';
+import { db, collection, getDocs } from '../../../config/firebase/firebase-key-config';
 
 let players = [
   {
     name: 'Dragos',
     role: 'rat',
     fakeid: 'Matei',
+    nrOfVotes: 0,
     vote: 'Mario',
-    nrOfVotes: 0, 
     score: 0
   },
   {
     name: 'Cosmin',
     role: 'cat',
     fakeid: 'Cosmin',
-    vote: 'Matei',
+    vote: 'Mario',
     nrOfVotes: 0, 
     score: 0
   }, 
@@ -47,140 +47,144 @@ let players = [
     name: 'Matei',
     role: 'rat',
     fakeid: 'Mario',
-    vote: 'Dragos',
     nrOfVotes: 0,
+    vote: 'Dragos',
     score: 0
   }
 ];
 
-function voteFor(playerName) {
-  let indexOfVoted = players.findIndex(player => player.fakeid == playerName);
-  players[indexOfVoted].votes++;
-  window.alert(players[indexOfVoted].votes);
-};
-
-function resetVotes() {
-  const nrOfPlayers = players.length;
-
-  for (let i = 0; i < nrOfPlayers; i++) {
-    players[i].votes = players[i].score = 0;
-  }
-}
-
-function roundUp10(num) {
-  return Math.ceil(num / 10) * 10;
-}
-
-function calculateScore() {
-  const nrOfPlayers = players.length;
-
-  for (let i = 0; i < nrOfPlayers; i++) {
-    let indexOfVoted = players.findIndex(player => player.fakeid == players[i].vote);
-
-    if (players[i].role === 'cat') { // real
-      if (players[indexOfVoted].role === 'rat') {
-        players[i].score += nrOfPlayers - 1 - players[indexOfVoted].nrOfVotes;
-      }
-      
-      if (players[i].score < 1) {
-          players[i].score = 1;
-      }
-      
-      players[i].score *= 10;
-      
-      if (players[i].score == 10 * (nrOfPlayers - 2)) {
-        players[i].score += 5;
-      }
-    } else { // fake
-      players[i].score += nrOfPlayers - 2 - players[i].nrOfVotes;
-      players[i].score *= 10;
-      
-      if (players[i].score == 10 * (nrOfPlayers - 2)){
-        players[i].score += 5;
-      }
-    }
-
-    players[i].score = roundUp10(players[i].score * 76 / nrOfPlayers);
-  }
-
-  window.alert("Scoreboard:\nDragos: " + players[0].score +
-                          "\nCosmin: " + players[1].score +
-                          "\nMario: " + players[2].score +
-                          "\nDavid: " + players[3].score + 
-                          "\nCostin: " + players[4].score + 
-                          "\nMatei: " + players[5].score);
-}
-
+let currentPlayerIndex = 3; // David
 
 export const ForgotPasswordPage = () => {
+  /*const [playersDB, setPlayersDB] = useState([]);
+
+  const getPlayers = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'games/abcd/players'));
+      let playersArray = [];
+  
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, ' => ', doc.data());
+        playersArray.push(doc.data());
+      });
+  
+      setPlayersDB(playersArray);
+    } catch (err) {
+      console.log('Error: ', err);
+    }
+  };*/
+
+  const voteFor = (playerName) => {
+    let indexOfVoted = players.findIndex(player => player.fakeid == playerName);
+    players[indexOfVoted].nrOfVotes++;
+    window.alert(players[indexOfVoted].nrOfVotes);
+  };
+  
+  const resetVotes = () => {
+    const nrOfPlayers = players.length;
+  
+    for (let i = 0; i < nrOfPlayers; i++) {
+      players[i].nrOfVotes = players[i].score = 0;
+    }
+  }
+  
+  const roundUp10 = (num) => {
+    return Math.ceil(num / 10) * 10;
+  }
+
+  const showScore = () => {
+    let results = "Scoreboard:";
+
+    players?.map((player) => {
+      results += "\n" + player.name + ": " + player.score;
+    });
+
+    window.alert(results);
+
+    showRats();
+
+    resetVotes();
+  }
+
+  const showRats = () => {
+    let rats = "The rats this round were:";
+
+    players?.map((player) => {
+      if (player.role == 'rat') {
+        rats += "\n" + player.name;
+      }
+    });
+
+    window.alert(rats);
+  }
+
+  const calculateScore = () => {
+    const nrOfPlayers = players.length;
+  
+    for (let i = 0; i < nrOfPlayers; i++) {
+      if (players[i].role === 'cat') { // real
+        let indexOfVoted = players.findIndex(player => player.fakeid == players[i].vote);
+
+        if (players[indexOfVoted].role === 'rat') {
+          players[i].score += nrOfPlayers - 1 - players[indexOfVoted].nrOfVotes;
+        }
+        
+        if (players[i].score < 1) {
+            players[i].score = 1;
+        }
+        
+        players[i].score *= 10;
+        
+        if (players[i].score == 10 * (nrOfPlayers - 2)) {
+          players[i].score += 5;
+        }
+      } else { // fake
+        players[i].score += nrOfPlayers - 2 - players[i].nrOfVotes;
+        players[i].score *= 10;
+        
+        if (players[i].score == 10 * (nrOfPlayers - 2)){
+          players[i].score += 5;
+        }
+      }
+  
+      players[i].score = roundUp10(players[i].score * 76 / nrOfPlayers);
+    }
+  
+    players.sort((a, b) => {
+      return (a.score < b.score);
+    });
+
+    showScore();
+  }
+   
+  // useEffect(() => {
+  //   getPlayers();
+  // }, []);
+
   return (
     <Center w="100%" h="100%" bgColor="coolGray.300">
       <Text paddingBottom="5px">Vote out the rats!</Text>
-      <Button
-        w="20%" h="5%"
-        marginBottom="4px"
-        padding="1px"
-        onPress={() => {
-          voteFor('Dragos');
-        }}
-      >
-        Dragos
-      </Button>
 
-      <Button
-        w="20%" h="5%"
-        marginBottom="4px"
-        padding="1px"
-        onPress={() => {
-          voteFor('Cosmin');
-        }}
-      >
-        Cosmin
-      </Button>
-
-      <Button
-        w="20%" h="5%"
-        marginBottom="4px"
-        padding="1px"
-        onPress={() => {
-          voteFor('Mario');
-        }}
-      >
-        Mario
-      </Button>
-
-      <Button
-        w="20%" h="5%"
-        marginBottom="4px"
-        padding="1px"
-        onPress={() => {
-          voteFor('David');
-        }}
-      >
-        David
-      </Button>
-
-      <Button
-        w="20%" h="5%"
-        marginBottom="4px"
-        padding="1px"
-        onPress={() => {
-          voteFor('Costin');
-        }}
-      >
-        Costin
-      </Button>
-
-      <Button
-        w="20%" h="5%"
-        marginBottom="4px"
-        padding="1px"
-        onPress={() => {
-          voteFor('Matei');
-        }}
-      >
-        Matei
-      </Button>
+      {
+        players?.map((player, index) => {
+          
+          if (index != currentPlayerIndex && player.name != players[currentPlayerIndex].fakeid )
+            return (
+              <Button
+                w="20%" h="5%"
+                marginBottom="4px"
+                padding="1px"
+                key={index}
+                onPress={() => {
+                  voteFor(player.name);
+                }}
+              >
+                {player.name}
+              </Button>
+            );
+        })
+      }
 
       <Button
         w="20%" h="5%"
