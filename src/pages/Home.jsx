5b7f1f501028ce6bd9c8ea97703b82842799157f
Cosmin Carsote <cosmin.c@adoreme.com>
2022-10-03 03:27:45 +0300

    more stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,105 @@
-import { View, Text, Image } from "react-native";
-import { Button, Flex } from "native-base";
-import React from "react";
+import React from 'react';
 
+import {
+  Text,
+  Image,
+  VStack,
+  Center,
+  Icon,
+  IconButton,
+  Box,
+  Button,
+  HStack,
+} from 'native-base';
+
+import { Ionicons } from '@expo/vector-icons';
+
+import {
+  auth,
+  db,
+  doc,
+  setDoc,
+  signOut,
+} from '../../config/firebase/firebase-key-config';
+
+import CopyratLogo from '../../assets/logo_trans.png';
 
 export const HomePage = ({ navigation }) => {
+  const logOut = () => {
+    signOut(auth).then(() => {
+      navigation.reset({
+        routes: [{ name: 'Login' }],
+      });
+    });
+  };
+
+  const generateRoomKey = async (length) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const charactersLength = characters.length;
+
+    const currentUser = auth.currentUser;
+
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    try {
+      await setDoc(doc(db, 'games', result), {
+        game_admin_uid: currentUser.uid,
+      });
+
+      navigation.navigate('Lobby', {
+        roomKey: result,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <View style={{ alignItems: "center", justifyContent: "center", flex: 1, backgroundColor: "#747474" }}>
-      <Image source={require('../../assets/logo_trans.png')} style = {{width:300, height: 300}} />
-      <Text style={{fontSize: 60, fontWeight: "bold"}} 
-      >copyrat</Text>
-      <Button style={{padding: 10, width: 120, height: 50, backgroundColor: '#74859a', borderRadius: 10, borderWidth: 2, borderColor: '#000000', margin: 100}}
-      onPress={() => navigation.navigate("Lobby")}>Lobby</Button>
-    </View>
+    <Box bg="primary1.500" h="100%" w="100%" position="relative">
+      <IconButton
+        position="absolute"
+        top="8"
+        left="8"
+        icon={<Icon as={<Ionicons name="settings-outline" />} />}
+        borderRadius="full"
+        _icon={{
+          color: 'white',
+          size: '8',
+        }}
+        _pressed={{
+          bg: 'primary3.600',
+        }}
+        onPress={logOut}
+      />
+
+      <VStack justifyContent="flex-start" alignItems="center" my="16">
+        <Image
+          alt="Copy Rat Logo"
+          source={CopyratLogo}
+          style={{ width: 150, height: 150 }}
+        />
+
+        <Text fontSize="5xl" fontFamily="RadioNewsman" color="black">
+          Copy Rat
+        </Text>
+      </VStack>
+
+      <Center w="full">
+        <HStack space="sm">
+          <Button
+            onPress={() => {
+              generateRoomKey(4);
+            }}
+          >
+            Create
+          </Button>
+
+          <Button>Join</Button>
+        </HStack>
+      </Center>
+    </Box>
   );
 };
 
--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -1,111 +1,135 @@
-import { View, Text, StyleSheet, Image } from "react-native";
-import React, { useEffect, useReducer, useState } from "react";
-import { Button } from "native-base";
+import React, { useEffect, useReducer, useState } from 'react';
 
-import {
-  collection,
-  db,
-  getDocs,
-} from '../../config/firebase/firebase-key-config';
-import { async } from "@firebase/util";
-import { doc, updateDoc, increment } from "firebase/firestore";
-import { ImageBackground } from "react-native";
+import { View, StyleSheet, Image } from 'react-native';
+import { Box, Button, Text } from 'native-base';
 
-let uIds = [];
+// import {
+//   collection,
+//   db,
+//   getDocs,
+// } from '../../config/firebase/firebase-key-config';
+// import { async } from '@firebase/util';
+// import { doc, updateDoc, increment } from 'firebase/firestore';
+import { ImageBackground } from 'react-native';
 
-export const LobbyPage = ({ navigation }) => {  
-  const [playersDB, setPlayersDB] = useState([]);
- 
-  const getPlayers = async () => {
-    try {
-      const querySnapshot = await getDocs(collection(db, 'games/abcd/players'));
-      let playersArray = [];
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, ' => ', doc.data());
-        playersArray.push(doc.data());
-        uIds.push(doc.id);
-      });
-      setPlayersDB(playersArray);
-    } catch (err) {
-      console.log('Error: ', err);
-    }
-  };
+// let uIds = [];
 
-  useEffect(() => {getPlayers()}, [])
-  
+export const LobbyPage = ({ navigation, route }) => {
+  // const [playersDB, setPlayersDB] = useState([]);
+  const { roomKey } = route.params;
+
+  // const getPlayers = async () => {
+  //   try {
+  //     const querySnapshot = await getDocs(collection(db, 'games/abcd/players'));
+  //     let playersArray = [];
+  //     querySnapshot.forEach((doc) => {
+  //       // doc.data() is never undefined for query doc snapshots
+  //       // console.log(doc.id, ' => ', doc.data());
+  //       playersArray.push(doc.data());
+  //       uIds.push(doc.id);
+  //     });
+  //     setPlayersDB(playersArray);
+  //   } catch (err) {
+  //     console.log('Error: ', err);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getPlayers();
+  // }, []);
+
   //Assign roles
-  const setRoles = () => {
-    let no_of_rats = playersDB.length / 2;   
-    //arr of 3 rand index
-    var arr = [];
-    while(arr.length < no_of_rats){
-      var r = Math.floor(Math.random() * (playersDB.length - 1)) + 1;
-      if(arr.indexOf(r) === -1) arr.push(r);
-    }
-    //update roles and fake_id
-    for(let i = 0; i < no_of_rats; i++) {
-      updateDoc(doc(db, 'games/abcd/players/' + uIds[arr[i]]), {
-          role: "rat" ,
-          fake_id: playersDB[arr[(i + 1) % no_of_rats]].name
-      });
-    }
-  }
+  // const setRoles = () => {
+  //   let no_of_rats = playersDB.length / 2;
+  //   //arr of 3 rand index
+  //   var arr = [];
+  //   while (arr.length < no_of_rats) {
+  //     var r = Math.floor(Math.random() * (playersDB.length - 1)) + 1;
+  //     if (arr.indexOf(r) === -1) arr.push(r);
+  //   }
+  //   //update roles and fake_id
+  //   for (let i = 0; i < no_of_rats; i++) {
+  //     updateDoc(doc(db, 'games/abcd/players/' + uIds[arr[i]]), {
+  //       role: 'rat',
+  //       fake_id: playersDB[arr[(i + 1) % no_of_rats]].name,
+  //     });
+  //   }
+  // };
 
   //reset roles and fake_id
-  const reset = () => {
-    for(let i = 0; i < playersDB.length; i++) {
-      updateDoc(doc(db, 'games/abcd/players/' + uIds[i]), {
-        role: "cat" ,
-        fake_id: playersDB[i].name
-    });
-    }
-  }
+  // const reset = () => {
+  //   for (let i = 0; i < playersDB.length; i++) {
+  //     updateDoc(doc(db, 'games/abcd/players/' + uIds[i]), {
+  //       role: 'cat',
+  //       fake_id: playersDB[i].name,
+  //     });
+  //   }
+  // };
 
   return (
-    <View style={{ alignItems: "center", justifyContent: "center", flex: 1, backgroundColor:"#747474" }}>
-      <ImageBackground 
-      source={require('../../assets/logo_trans.png')} 
-      style = {{width:500, height: 500, backgroundColor: 'transparent', opacity: 0.05}} />
-      <Text style={{fontSize: 30, fontWeight: 'bold'}}>Lobby Page</Text>
-      <Button style={styles.buttonStyle}
-        onPress={() => {
-          setRoles();
-          navigation.navigate("Chat")
-        }
-        }>Start
-      </Button>
-      
-      <Button style={styles.buttonStyle}
-        onPress={() => {
-          reset();
-        }
-      }>Reset
-      </Button>
-    </View>
+    // <View
+    //   style={{
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //     flex: 1,
+    //     backgroundColor: '#747474',
+    //   }}
+    // >
+    //   <ImageBackground
+    //     source={require('../../assets/logo_trans.png')}
+    //     style={{
+    //       width: 500,
+    //       height: 500,
+    //       backgroundColor: 'transparent',
+    //       opacity: 0.05,
+    //     }}
+    //   />
+    //   <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Lobby Page</Text>
+    //   <Button
+    //     style={styles.buttonStyle}
+    //     onPress={() => {
+    //       setRoles();
+    //       navigation.navigate('Chat');
+    //     }}
+    //   >
+    //     Start
+    //   </Button>
+
+    //   <Button
+    //     style={styles.buttonStyle}
+    //     onPress={() => {
+    //       reset();
+    //     }}
+    //   >
+    //     Reset
+    //   </Button>
+    // </View>
+    <Box>
+      <Text>{roomKey}</Text>
+    </Box>
   );
 };
 
-const styles = StyleSheet.create({
-  input: {
-    padding: 10,
-    margin: 4,
-    width: "75%",
-    fontSize: 15,
-    backgroundColor: "#74859a",
-    borderRadius: 10,
-    margin: 5,
-  },
-  buttonStyle: {
-    padding: 5,
-    width: 120,
-    height: 50,
-    backgroundColor: '#74859a',
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: '#000000',
-    margin: 10
-  }
-});
+// const styles = StyleSheet.create({
+//   input: {
+//     padding: 10,
+//     margin: 4,
+//     width: '75%',
+//     fontSize: 15,
+//     backgroundColor: '#74859a',
+//     borderRadius: 10,
+//     margin: 5,
+//   },
+//   buttonStyle: {
+//     padding: 5,
+//     width: 120,
+//     height: 50,
+//     backgroundColor: '#74859a',
+//     borderRadius: 10,
+//     borderWidth: 2,
+//     borderColor: '#000000',
+//     margin: 10,
+//   },
+// });
 
 export default LobbyPage;
