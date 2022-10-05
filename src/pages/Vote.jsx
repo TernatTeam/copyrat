1b471622add31f055d209ccb21fadbf2ff97655e
MarioNegoita <58939386+MarioNegoita@users.noreply.github.com>
2022-10-05 22:40:34 +0300

    VotePageStyle

--- src/pages/Vote.jsx
+++ src/pages/Vote.jsx
@@ -1,24 +1,25 @@
-import {View, Text} from 'react-native';
-import {Button} from 'native-base';
-import React, {useEffect, useState} from 'react';
+import { View, Text, TouchableOpacity } from "react-native";
+import { Box, Button, Center } from "native-base";
+import React, { useEffect, useState } from "react";
 import {
   doc,
   getDoc,
   updateDoc,
   deleteDoc,
-  increment
-} from 'firebase/firestore';
+  increment,
+} from "firebase/firestore";
 import {
   db,
   collection,
   auth,
-  getDocs
-} from '../../config/firebase/firebase-key-config';
-import {useGlobal} from '../../state';
+  getDocs,
+} from "../../config/firebase/firebase-key-config";
+import { useGlobal } from "../../state";
+import { Ionicons } from "@expo/vector-icons";
+import { Icon } from "native-base";
 
-export const VotePage = ({navigation}) => {
-
-/* variabile carora li se modifica valoarea pe parcursul jocului:
+export const VotePage = ({ navigation }) => {
+  /* variabile carora li se modifica valoarea pe parcursul jocului:
   -> playersDB = array ul cu playeri, luat din baza de date
   -> uIDs = arrayul cu id-urile playerilor, in cazul in care un player iese din joc
   -> indexOfVoted = indexul playerului cu care doresti sa votezi, pana confirmi votul
@@ -30,7 +31,7 @@ export const VotePage = ({navigation}) => {
   const [indexOfVoted, setIndexOfVoted] = useState(null);
   const [alreadyVoted, setAlreadyVoted] = useState(false);
 
-/* variabile carora nu li se modifica valoarea pe parcursul jocului:
+  /* variabile carora nu li se modifica valoarea pe parcursul jocului:
   -> keycode = retine cheia camerei de joc in "keycode.value"
   -> currentPlayer = odata initializat, retine informatiile despre playerul curent
   -> adminId = id ul playerului care a creat camera
@@ -43,63 +44,82 @@ export const VotePage = ({navigation}) => {
   const getPlayers = async () => {
     try {
       // iau playerii jocului curent
-      const querySnapshot = await getDocs(collection(db, `games/${roomData.keyCode}/players`));
+      const querySnapshot = await getDocs(
+        collection(db, `games/${roomData.keyCode}/players`)
+      );
       // ii pun in arrayul playersArray pentru a updata valoarea arrayului playersDB
       let playersArray = [];
       // la fel si pentru id ul acestora
       let idArray = [];
 
-      querySnapshot.forEach((doc) => { // pentru fiecare player luat din baza de data
-        playersArray.push(doc.data()); // il retin in array ul auxiliar       
+      querySnapshot.forEach((doc) => {
+        // pentru fiecare player luat din baza de data
+        playersArray.push(doc.data()); // il retin in array ul auxiliar
         idArray.push(doc.id); // retinem si id urile playerilor in array ul auxiliar
-     
-        if (doc.id == auth.currentUser.uid) { // daca am gasit playeryl cu id ul curent, il retinem in
-         setCurrentPlayer(doc.data());    // variabila currentPlayer pentru a avea acces usor la date
-        }                                // cand cream butoanele de votare
+
+        if (doc.id == auth.currentUser.uid) {
+          // daca am gasit playeryl cu id ul curent, il retinem in
+          setCurrentPlayer(doc.data()); // variabila currentPlayer pentru a avea acces usor la date
+        } // cand cream butoanele de votare
       });
 
       setPlayersDB(playersArray); // modific arrayul in care tin minte playerii
       setPlayerIDs(idArray); // modific arrayul in care tin minte id urile
-    } catch(err) {
+    } catch (err) {
       console.log(`Error: ${err}`);
     }
-  }
+  };
 
-  const getAdminId = async () => { // functie care retine id ul adminului, se apeleaza o data la
+  const getAdminId = async () => {
+    // functie care retine id ul adminului, se apeleaza o data la
     const docSnap = await getDoc(doc(db, `games/${roomData.keyCode}`)); // prima incarcare a paginii
     setAdminId(docSnap.data().game_admin_uid);
-  }
+  };
 
-  const voteFor = (index) => { // retine ultima optiune de votare a playerului curent
-    setIndexOfVoted(index);    // in variabila indexOfVoted
+  const voteFor = (index) => {
+    // retine ultima optiune de votare a playerului curent
+    setIndexOfVoted(index); // in variabila indexOfVoted
 
     window.alert(`You are voting for ${playersDB[index].fake_id}`);
-  }
+  };
 
-  const confirmVote = async () => { 
-  // functia care trimite catre baza de date indicele persoanei cu care votezi
-  // si ii creste acestuia numarul de voturi primite
-    await updateDoc(doc(db, `games/${roomData.keyCode}/players/${playerIDs[indexOfVoted]}`), {
-      no_of_votes: increment(1)
-    });
+  const confirmVote = async () => {
+    // functia care trimite catre baza de date indicele persoanei cu care votezi
+    // si ii creste acestuia numarul de voturi primite
+    await updateDoc(
+      doc(db, `games/${roomData.keyCode}/players/${playerIDs[indexOfVoted]}`),
+      {
+        no_of_votes: increment(1),
+      }
+    );
 
-    await updateDoc(doc(db, `games/${roomData.keyCode}/players/${auth.currentUser.uid}`), {
-      vote: indexOfVoted
-    });
+    await updateDoc(
+      doc(db, `games/${roomData.keyCode}/players/${auth.currentUser.uid}`),
+      {
+        vote: indexOfVoted,
+      }
+    );
     getPlayers();
-  }
+  };
 
-  const calculateScore = async () => { // functia care calculeaza punctajele la final de runda
+  const calculateScore = async () => {
+    // functia care calculeaza punctajele la final de runda
     let newScores = []; // array auxiliar in care stocam scorurile de runda asta
     const nrOfPlayers = playersDB.length;
 
     console.log(playersDB);
-    
-    for (let i = 0; i < nrOfPlayers; i++) { // calculam pentru fiecare player
+
+    for (let i = 0; i < nrOfPlayers; i++) {
+      // calculam pentru fiecare player
       let score = 0;
-      
-      if (playersDB[i].name == playersDB[i].fake_id) { // Real
-        if (playersDB[playersDB[i].vote].name != playersDB[playersDB[i].vote].fake_id) { //vot bun
+
+      if (playersDB[i].name == playersDB[i].fake_id) {
+        // Real
+        if (
+          playersDB[playersDB[i].vote].name !=
+          playersDB[playersDB[i].vote].fake_id
+        ) {
+          //vot bun
           score += nrOfPlayers - 1 - playersDB[playersDB[i].vote].no_of_votes;
         }
         if (score < 1) {
@@ -109,11 +129,11 @@ export const VotePage = ({navigation}) => {
         if (score == 10 * (nrOfPlayers - 2)) {
           score += 5;
         }
-        
-      } else {// fake
+      } else {
+        // fake
         score += nrOfPlayers - 2 - playersDB[i].no_of_votes;
         score *= 10;
-        if (score == (nrOfPlayers - 2)) {
+        if (score == nrOfPlayers - 2) {
           score += 5;
         }
       }
@@ -121,113 +141,185 @@ export const VotePage = ({navigation}) => {
       score = (score * 76) / nrOfPlayers;
       score = Math.ceil(score / 10) * 10;
 
-      newScores.push(score);  // incarc in vectorul auxiliar fiecare scor nou
-    
+      newScores.push(score); // incarc in vectorul auxiliar fiecare scor nou
+
       //console.log(playersDB[i].name, score);
     }
-    
-    for (let i = 0; i < nrOfPlayers; i++) { // si apoi il actualizez in baza de date pentru fiecare jucator
-      await updateDoc(doc(db, `games/${roomData.keyCode}/players/${playerIDs[i]}`), {
-        score: increment(newScores[i])
-      });
+
+    for (let i = 0; i < nrOfPlayers; i++) {
+      // si apoi il actualizez in baza de date pentru fiecare jucator
+      await updateDoc(
+        doc(db, `games/${roomData.keyCode}/players/${playerIDs[i]}`),
+        {
+          score: increment(newScores[i]),
+        }
+      );
     }
-  }
+  };
 
-  const showRats = () => { // functia care afiseaza jucatorii care au avut rolul de "rat"
-    let rats = 'The rats this round were:';
+  const showRats = () => {
+    // functia care afiseaza jucatorii care au avut rolul de "rat"
+    let rats = "The rats this round were:";
 
-    playersDB.map((player) => { // verificam prin array ul de playeri
+    playersDB.map((player) => {
+      // verificam prin array ul de playeri
       if (player.name != player.fake_id) {
-        rats += '\n' + player.name; // retinem numele celor cu numele si fake_id ul diferit
+        rats += "\n" + player.name; // retinem numele celor cu numele si fake_id ul diferit
       }
     });
 
     window.alert(rats);
-  }
+  };
 
   useEffect(() => {
     getPlayers(); // cand se incarca prima data pagina, luam din baza de date
     getAdminId(); // playerii si id urile lor, cat si pe al admin ului
-  }, []);         
+  }, []);
 
   return (
-    <View style = {{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Text paddingBottom="5px">Vote out the rats!</Text>
-      
-      { // generare butoane de votare
-        playersDB?.map((player, index) => { // parcurg array ul de playeri
-          if (player.name != currentPlayer.name &&
-              player.name != currentPlayer.fake_id) { // verificare sa nu fie playerul curent
-            return (                                  // (sau ala drept care se da, in cazul ratilor)
-              <Button
-                w="20%"
-                h="5%"
-                marginBottom="4px"
-                padding="1px"
+    <Center
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+        backgroundColor: "#747474",
+      }}
+    >
+      <Text style={{ fontFamily: "RadioNewsman", fontSize: 25 }}>
+        Vote out the rats!
+      </Text>
+
+      {
+        // generare butoane de votare
+        playersDB?.map((player, index) => {
+          // parcurg array ul de playeri
+          if (
+            player.name != currentPlayer.name &&
+            player.name != currentPlayer.fake_id
+          ) {
+            // verificare sa nu fie playerul curent
+            return (
+              // (sau ala drept care se da, in cazul ratilor)
+              // <Button
+              //   w="20%"
+              //   h="5%"
+              //   marginBottom="4px"
+              //   padding="1px"
+              //   key={index}
+              //   onPress={() => {
+              //     // la apasare, se apeleaza functia care
+              //     voteFor(index); // retine ultima optiune de votare a playerului curent
+              //   }}
+              // >
+              //   {player.fake_id}
+              // </Button>
+              <TouchableOpacity
                 key={index}
-                onPress={() => {  // la apasare, se apeleaza functia care
+                onPress={() => {
+                  //     // la apasare, se apeleaza functia care
                   voteFor(index); // retine ultima optiune de votare a playerului curent
                 }}
+                style={{ width: "80%", margin: 10, marginTop: 15 }}
               >
-                {player.fake_id}
-              </Button>
+                <Box
+                  style={{
+                    backgroundColor: player.userNameColor,
+                    paddingVertical: 5,
+                    borderRadius: 10,
+                    justifyContent: "space-between",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: "white",
+                      fontSize: 22,
+                      marginLeft: 10,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {player.fake_id}
+                  </Text>
+
+                  <Icon
+                    as={<Ionicons name="checkbox" />}
+                    size={6}
+                    mr="2"
+                    color={"white"}
+                  />
+                </Box>
+              </TouchableOpacity>
             );
           }
         })
       }
 
       <Button // butonul cu dare confirmi votul final
-        w="20%"
-        h="5%"
-        marginBottom="4px"
-        padding="1px"
-        bgColor="fuchsia.700"
+        title="LockIn"
+        marginTop={6}
+        marginBottom={3}
+        rounded="lg"
+        medium
+        bg="primary3.500"
+        _pressed={{ bg: "primary3.600" }}
         onPress={() => {
-        // la apasare, se apeleaza functia care trimite catre baza de date indicele persoanei cu care votezi
-          if (indexOfVoted != null) { // doar daca playerul a votat cu cineva. Atunci, poti
-            if (!alreadyVoted) {      // confirma votul o singura data, adica daca nu ai mai apasat
-              confirmVote();          // pe "Done" (variabila alreadyVoted are valoarea false)
-              setAlreadyVoted(true); 
+          // la apasare, se apeleaza functia care trimite catre baza de date indicele persoanei cu care votezi
+          if (indexOfVoted != null) {
+            // doar daca playerul a votat cu cineva. Atunci, poti
+            if (!alreadyVoted) {
+              // confirma votul o singura data, adica daca nu ai mai apasat
+              confirmVote(); // pe "Done" (variabila alreadyVoted are valoarea false)
+              setAlreadyVoted(true);
               window.alert(`Locking in ... ${playersDB[indexOfVoted].fake_id}`);
             } else {
-              window.alert('You already locked in your vote!');
+              window.alert("You already locked in your vote!");
             }
           } else {
-            window.alert('Please vote for someone!'); // in caz contrar, anunt playerul
-          }                                         // ca nu a votat cu nimeni
+            window.alert("Please vote for someone!"); // in caz contrar, anunt playerul
+          } // ca nu a votat cu nimeni
         }}
-      >Done</Button>
+      >
+        <Text style={{ fontWeight: "semibold", color: "black", fontSize: 16 }}>
+          Lock In Your Vote!
+        </Text>
+      </Button>
 
       <Button
-        w="20%"
-        h="5%"
-        marginBottom="4px"
-        padding="1px"
-        bgColor="emerald.600"
-        onPress={() => { // butonul care calculeaza si afiseaza scorurile, si da update in baza de date
-          if (auth.currentUser.uid == adminId) { // acest lucru e posibil doar daca playerul care apasa are rolul de admin
+        title="stopVote"
+        rounded="lg"
+        medium
+        bg="rosybrown"
+        _pressed={{ bg: "red.500" }}
+        onPress={() => {
+          // butonul care calculeaza si afiseaza scorurile, si da update in baza de date
+          if (auth.currentUser.uid == adminId) {
+            // acest lucru e posibil doar daca playerul care apasa are rolul de admin
             getPlayers(); // actualizam arrayul de playeri
 
-            calculateScore(); // calculam scorurile                    
-            
+            calculateScore(); // calculam scorurile
+
             setTimeout(() => {
               showRats(); // apoi afisam ratii de tura aceasta
             }, 1000);
-  
+
             setTimeout(() => {
-              window.alert('Calculating scores...');
+              window.alert("Calculating scores...");
             }, 3000);
-  
+
             setTimeout(() => {
-              navigation.navigate('Scoreboard'); // ne mutam pe pagina cu leaderboard ul
+              navigation.navigate("Scoreboard"); // ne mutam pe pagina cu leaderboard ul
             }, 5000);
           } else {
-            window.alert('Wait! Only the game creator can stop the voting.'); // altfel, este anuntat ca
-          }                                                                   // nu are acest drept
+            window.alert("Wait! Only the game creator can stop the voting."); // altfel, este anuntat ca
+          } // nu are acest drept
         }}
-      >Stop Vote!</Button>
-
-    </View>
+      >
+        <Text style={{ fontWeight: "semibold", color: "black", fontSize: 16 }}>
+          Stop Vote!
+        </Text>
+      </Button>
+    </Center>
   );
 };
 
