99c3271f650522cbd36e97a8e26640fd90cd0bd5
Cosmin Carsote <cosmin.c@adoreme.com>
2022-10-05 18:13:58 +0300

    aaaand done lobby i think

--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -7,6 +7,8 @@ import {
   Heading,
   Icon,
   IconButton,
+  ScrollView,
+  Skeleton,
   Text,
   useToast,
   VStack,
@@ -21,7 +23,6 @@ import {
   collection,
   db,
   doc,
-  getDocs,
   query,
   updateDoc,
   onSnapshot,
@@ -29,7 +30,6 @@ import {
 } from '../../config/firebase/firebase-key-config';
 
 import { useGlobal } from '../../state';
-import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { ModalName } from '../components/common';
 
 let uIds = [];
@@ -37,12 +37,12 @@ let uIds = [];
 export const LobbyPage = ({ navigation }) => {
   const [players, setPlayers] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(true);
-  const [isFirstLoad, setIsFirstLoad] = useState(true);
+  const [hasLeft, setHasLeft] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingButton, setIsLoadingButton] = useState(false);
   const [{ roomData }] = useGlobal();
   const userNameColors = [
     'lightblue',
-    'black',
-    'antiquewhite',
     'aqua',
     'purple',
     'darkmagenta',
@@ -72,72 +72,51 @@ export const LobbyPage = ({ navigation }) => {
 
   //Assign roles
   const setRoles = async () => {
+    setIsLoadingButton(true);
+
     //set index
-    for (let i = 0; i < playersDB.length; i++) {
+    for (let i = 0; i < players.length; i++) {
       await updateDoc(doc(db, `games/${roomData.keyCode}/players/` + uIds[i]), {
         index: i,
       });
     }
 
-    let no_of_rats = Math.floor(playersDB.length / 2);
+    let no_of_rats = Math.floor(players.length / 2);
 
     //arr of 3 rand index
     var arr = [];
 
     while (arr.length < no_of_rats) {
-      var r = Math.floor(Math.random() * playersDB.length);
+      var r = Math.floor(Math.random() * players.length);
       if (arr.indexOf(r) === -1) arr.push(r);
     }
-    console.log(arr);
+
     //update roles and fake_id
     for (let i = 0; i < no_of_rats; i++) {
       await updateDoc(
         doc(db, `games/${roomData.keyCode}/players/` + uIds[arr[i]]),
         {
           role: 'rat',
-          fake_id: playersDB[arr[(i + 1) % no_of_rats]].name,
+          fake_id: players[arr[(i + 1) % no_of_rats]].name,
         },
       );
     }
-  };
 
-  //reset roles and fake_id
-  // const reset = async () => {
-  //   for (let i = 0; i < playersDB.length; i++) {
-  //     await updateDoc(doc(db, `games/${roomData.keyCode}/players/` + uIds[i]), {
-  //       role: 'cat',
-  //       fake_id: playersDB[i].name,
-  //     });
-  //   }
-  // };
+    setIsLoadingButton(false);
+  };
 
   useEffect(() => {
     const q = query(collection(db, `games/${roomData.keyCode}/players`));
     const unsubscribe = onSnapshot(q, (snapshot) => {
       snapshot.docChanges().forEach((change) => {
         if (change.type === 'added') {
-          setPlayers((oldValues) => [...oldValues, change.doc.data()]);
+          setPlayers((oldValues) => [
+            ...oldValues,
+            { ...change.doc.data(), uid: change.doc.id },
+          ]);
           uIds.push(change.doc.id);
 
-          if (isFirstLoad === false) {
-            console.log('ceva');
-            toast.show({
-              id,
-              duration: 2500,
-              placement: 'top',
-              render: () => {
-                return (
-                  <Box bg="green.500" px="2" py="1" rounded="sm" mb={4}>
-                    {change.doc.data().name} joined the room
-                  </Box>
-                );
-              },
-            });
-          }
-        }
-
-        if (change.type === 'modified') {
-          // console.log('baga', change.doc.data());
+          setIsLoading(false);
         }
 
         if (change.type === 'removed') {
@@ -147,120 +126,189 @@ export const LobbyPage = ({ navigation }) => {
             ),
           );
 
-          if (!toast.isActive(id)) {
-            toast.show({
-              id,
-              duration: 2500,
-              placement: 'top',
-              render: () => {
-                return (
-                  <Box flex={1} bg="red.500" px="2" py="1" rounded="sm" mb={4}>
-                    <Text>{change.doc.data().name} left the room</Text>
-                  </Box>
-                );
-              },
-            });
-          }
+          setHasLeft({ id: change.doc.id, name: change.doc.data().name });
+
+          setIsLoading(false);
         }
       });
     });
 
-    setIsFirstLoad(false);
-
     return async () => {
       unsubscribe();
-      try {
-        await deleteDoc(
-          doc(db, `games/${roomData.keyCode}/players`, auth.currentUser.uid),
-        );
-      } catch (err) {
-        console.log('Err: ', err);
-      }
     };
   }, []);
 
   useEffect(() => {
-    console.log(isFirstLoad);
-  }, [isFirstLoad]);
+    if (hasLeft) {
+      if (!toast.isActive(hasLeft.id)) {
+        toast.show({
+          id: hasLeft.id,
+          duration: 2500,
+          placement: 'top',
+          render: () => {
+            return (
+              <Box flex={1} bg="red.500" px="2" py="1" rounded="sm" mb={4}>
+                <Text>{hasLeft.name} left the room</Text>
+              </Box>
+            );
+          },
+        });
+      }
+    }
+  }, [hasLeft]);
+
+  const PlayersLoader = () => {
+    const loaderArray = [];
+    for (let i = 0; i <= 10; i++) {
+      loaderArray.push(
+        <Box key={i} padding={2}>
+          <Skeleton borderRadius={10} />
+        </Box>,
+      );
+    }
+
+    return loaderArray;
+  };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <Box safeArea bg="primary1.500" h="100%" w="100%">
-        <ModalName
-          show={isModalOpen}
-          onClose={() => {
-            setIsModalOpen(false);
+    <Box safeArea bg="primary1.500" h="100%" w="100%">
+      <ModalName
+        show={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(false);
+        }}
+        userNameColors={userNameColors}
+        keyCode={roomData.keyCode}
+      />
+
+      <Box px="2" w="full" justifyContent="center" alignItems="flex-start">
+        <IconButton
+          icon={<Icon as={<Ionicons name="arrow-back-outline" />} />}
+          borderRadius="full"
+          _icon={{
+            color: 'white',
+            size: '8',
           }}
-          userNameColors={userNameColors}
-          keyCode={roomData.keyCode}
+          _pressed={{
+            bg: 'primary3.600',
+          }}
+          onPress={async () => {
+            try {
+              await deleteDoc(
+                doc(
+                  db,
+                  `games/${roomData.keyCode}/players`,
+                  auth.currentUser.uid,
+                ),
+              );
+
+              navigation.reset({
+                routes: [{ name: 'Home' }],
+              });
+            } catch (err) {
+              console.log('Err: ', err);
+            }
+          }}
         />
-
-        <Box w="full" alignItems="center" justifyContent="center" p="12">
-          <Heading size="2xl" fontWeight="600" color="black">
-            Room key:
-          </Heading>
-
-          <Flex
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            mb="12"
-          >
-            <Text
-              textAlign="center"
-              fontSize="3xl"
-              fontFamily="RadioNewsman"
-              color="black"
-            >
-              {roomData.keyCode}
-            </Text>
-
-            <IconButton
-              icon={<Icon as={<Ionicons name="copy-outline" />} />}
-              borderRadius="full"
-              _icon={{
-                color: 'white',
-                size: '5',
-              }}
-              _pressed={{
-                bg: 'primary3.600',
-              }}
-              onPress={copyToClipboard}
-            />
-          </Flex>
-        </Box>
-
-        <Box w="full" px="16">
-          <VStack>
-            {auth.currentUser.uid == roomData.game_admin_uid && (
-              <Button
-                style={{ margin: 20 }}
-                onPress={() => {
-                  if (auth.currentUser.uid == roomData.game_admin_uid) {
-                    setRoles();
-                    // navigation.navigate('Chat');
-                  }
-                }}
-              >
-                Start
-              </Button>
-            )}
-
-            {/* <Button
-          style={{ margin: 100 }}
-          onPress={() => {
-            reset();
-          }}
-        >
-          Reset
-        </Button> */}
-            {players.map((player, index) => {
-              return <Text key={index}>{player.name}</Text>;
-            })}
-          </VStack>
-        </Box>
       </Box>
-    </TouchableWithoutFeedback>
+
+      <Box w="full" alignItems="center" justifyContent="center">
+        <Heading size="2xl" fontWeight="600" color="black">
+          Room key:
+        </Heading>
+
+        <Flex
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          mb="12"
+        >
+          <Text
+            textAlign="center"
+            fontSize="3xl"
+            fontFamily="RadioNewsman"
+            color="black"
+          >
+            {roomData.keyCode}
+          </Text>
+
+          <IconButton
+            icon={<Icon as={<Ionicons name="copy-outline" />} />}
+            borderRadius="full"
+            _icon={{
+              color: 'white',
+              size: '5',
+            }}
+            _pressed={{
+              bg: 'primary3.600',
+            }}
+            onPress={copyToClipboard}
+          />
+        </Flex>
+      </Box>
+
+      <Box px="6">
+        <Heading size="xl" fontWeight="600" color="black">
+          Players:
+        </Heading>
+      </Box>
+
+      <Box px="4" mt="2" mb="12" h="50%">
+        <ScrollView w="full">
+          {isLoading ? (
+            <PlayersLoader />
+          ) : (
+            <VStack flex="1" justifyContent="center" alignItems="center" px="2">
+              {players.map((player, index) => {
+                return (
+                  <Flex
+                    key={index}
+                    direction="row"
+                    justify="space-between"
+                    align="center"
+                    bg={player.userNameColor}
+                    padding={3}
+                    borderRadius={10}
+                    my="2"
+                    w="full"
+                  >
+                    <Text fontWeight="bold">{player.name}</Text>
+                    {auth.currentUser.uid == player.uid && (
+                      <Text fontWeight="extrabold">Room Admin</Text>
+                    )}
+                  </Flex>
+                );
+              })}
+            </VStack>
+          )}
+        </ScrollView>
+      </Box>
+
+      <Box px="6">
+        {auth.currentUser.uid == roomData.game_admin_uid && (
+          <Button
+            onPress={() => {
+              if (auth.currentUser.uid == roomData.game_admin_uid) {
+                setRoles();
+                navigation.navigate('Chat');
+              }
+            }}
+            title="Start"
+            rounded="lg"
+            medium
+            bg="primary3.500"
+            _pressed={{ bg: 'primary3.600' }}
+            disabled={isLoadingButton}
+            isLoading={isLoadingButton}
+            _spinner={{ paddingY: '0.45' }}
+          >
+            <Text fontWeight="semibold" color="black">
+              Start
+            </Text>
+          </Button>
+        )}
+      </Box>
+    </Box>
   );
 };
 
