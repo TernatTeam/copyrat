1768375674fa577283cb284d4fb90e353cb60a29
DragosRadut <dragosandreiradut@gmail.com>
2022-10-22 21:11:42 +0300

    again. Co-authored-by: VladCostin4 vldcostin@gmail.com

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export const Rules = () => {
   return (
     <View alignItems="center" justifyContent="center" flex={1}>
-      <Text style={{ fontSize: 25 }}>Regulile sunt pe drum</Text>
+      <Text style={{ fontSize: 25 }}>Regulile sunt pe drum...</Text>
     </View>
   );
 };
