da1c354a96326a0b5001f951518100fbf2b6acdf
Vlad Costin Andrei <vldcostin@gmail.com>
2022-10-25 12:27:39 +0300

    gata rulebook - de schimbat font

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -1,28 +1,31 @@
-import {
-  Text,
-  Icon,
-  IconButton,
-  Flex,
-  HStack,
-  VStack,
-} from "native-base";
+import { Text, Icon, IconButton, Flex, HStack, VStack, Box } from 'native-base';
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
+
+import { Audio } from 'expo-av';
 
 export const Rules = ({ navigation }) => {
   const [page, setPage] = useState(1);
+  
+  const playSound = async () => {
+    await Audio.setAudioModeAsync({ playsInSilentModeIOS: true });
 
+    const { sound: playbackObject } = await Audio.Sound.createAsync(
+      { uri: 'https://www.soundjay.com/misc/page-flip-02.mp3' },
+      { shouldPlay: true, rate: 1.5 },
+    );
+  }
+  
   return (
     <Flex safeArea bg="primary1.500" h="100%" w="100%" alignItems="center">
       <HStack
-          px="4"
-          w="full"
-          justifyContent="space-between"
-          alignItems="flex-start"
-        >
-        
+        px="4"
+        w="full"
+        justifyContent="space-between"
+        alignItems="flex-start"
+      >
         <IconButton
           icon={<Icon as={<Ionicons name="arrow-back-outline" />} />}
           borderRadius="full"
@@ -39,89 +42,99 @@ export const Rules = ({ navigation }) => {
             });
           }}
         />
-
       </HStack>
 
       <VStack p="5" justifyContent="center" alignItems="center" m="auto">
-        {page == 1 ? 
-        <Text color="white" fontSize="20" textAlign="center">
-          You are going to play a three round game, in which you will chat with your fellow players, 
-          but there's a catch. There are two types of players, rats and cats.
-        </Text> : null}
-        
+        {page == 1 ? (
+          <Text color="white" fontSize="17" fontFamily="RadioNewsman" textAlign="center">
+            Simple as meow. Three rounds, one chat and o lot of copyrats.{"\n"}{"\n"}
+	          Each round, you'll be assigned to be just like someone from the lobby.
+            You'll see your role in the top right of your screen:{"\n"} 'Playing as ...'.{"\n"}{"\n"}
+            You could either play as yourself (meaning you're a cat), either as one of your friends (making you a rat).
+          </Text>  
+        ) : null}
 
-        {page == 2 ?
-        <Text color="white" fontSize="20" textAlign="center">
-          Each round, half of the players will have to impersonate someone from the lobby. They will 
-          have assigned the rat role, and appear with a different name while chatting with the others. A text 
-          will appear in the top right of your screen saying 'Playing as ..', insert name of another rat player.
-        </Text> : null}
+        {page == 2 ? (
+          <Text color="white" fontSize="17" fontFamily="RadioNewsman" textAlign="center">
+            Let's say you're a cat. Try and catch the impostors among you ;). 
+            Ask personal questions, test their memory, see their response, do the math then pick a rat.{"\n"}{"\n"}
+	          As a rat, be a copy of the one you've been assigned. When texting, it will appear like it's them talking, 
+            so don't panic when you see yourself in there saying something you didn't.{"\n"}{"\n"}
+            Chat like the one you've been assigned, quote them, do anything just to fool the rest.
+            You can vote as well, so try to catch the other rats.
+          </Text>
+        ) : null}
 
-        {page == 3 ?
-        <Text color="white" fontSize="20" textAlign="center">
-          If you are not part of the chosen few, fear not, because you have the cat role. Your mission 
-          as cats is to try and catch the impostors among you. You can do that by carefully reading the 
-          messages that players write, if you think that someone said something that they wouldn't normally say.
-        </Text> : null}
-
-        {page == 4 ?
-        <Text color="white" fontSize="20" textAlign="center">
-          The rats get points by fooling the others and having as little players as possible vote for 
-          the, whereas the cats get points by correctly voting out the rats.
-        </Text> : null}
-
-        {page == 5 ?
-        <Text color="white" fontSize="20" textAlign="center">
-          Good luck!
-        </Text> : null}
+        {page == 3 ? (
+          <Text color="white" fontSize="17" fontFamily="RadioNewsman" textAlign="center">
+            Let's see the scoring!{"\n"}
+	          Well, the rats get points by fooling the others and having as little players as possible vote for them.{"\n"}{"\n"} 
+            Rats will be rats, so their vote doesn't add to their score, but it makes the others gain less.{"\n"}{"\n"} 
+	          Cats get points by correctly voting out the rats. Avoid teamming up, cause cats split points when voting for the same rat.{"\n"}{"\n"}
+	          Bonus points for anyone who does his job perfectly.{"\n"}{"\n"}{"\n"}
+            Good luck !
+          </Text>
+        ) : null}
       </VStack>
 
-      <HStack 
+      <HStack
         px="4"
         py="4"
         w="full"
         justifyContent="space-between"
         alignItems="flex-end"
       >
-        {page > 1 ?
-        <IconButton
-          icon={<Icon as={<Ionicons name="arrow-undo" />} />}
-          borderRadius="full"
-          _icon={{
-            color: 'white',
-            size: '8',
-          }}
-          _pressed={{
-            bg: 'primary3.600',
-          }}
-          onPress={() => {
-            if (page > 1) {
-            setPage(page - 1);
-            }
-          }}
-        /> : null}
+        {page > 1 ? (
+          <IconButton
+            icon={<Icon as={<Ionicons name="arrow-undo" />} />}
+            borderRadius="full"
+            _icon={{
+              color: 'white',
+              size: '8',
+            }}
+            _pressed={{
+              bg: 'primary3.600',
+            }}
+            onPress={() => {
+              playSound();
+              setTimeout(() => {
+                if (page > 1) {
+                  setPage(page - 1);
+                }  
+              }, 200);
+            }}
+          />
+        ) : (
+          <Box w="12"></Box>
+        )}
 
-        <Text color="white" fontSize="20" alignItems="center">
-          {page} / 5
+        <Text color="white" fontSize="15" fontFamily="RadioNewsman" alignItems="center" pb="2">
+          {page} / 3
         </Text>
 
-        {page < 5 ?
-        <IconButton
-          icon={<Icon as={<Ionicons name="arrow-redo" />} />}
-          borderRadius="full"
-          _icon={{
-            color: 'white',
-            size: '8',
-          }}
-          _pressed={{
-            bg: 'primary3.600',
-          }}
-          onPress={() => {
-            if (page < 5) {
-              setPage(page + 1);
-            }
-          }}
-        /> : null}
+        {page < 3 ? (
+          <IconButton
+            icon={<Icon as={<Ionicons name="arrow-redo" />} />}
+            borderRadius="full"
+            _icon={{
+              color: 'white',
+              size: '8',
+            }}
+            _pressed={{
+              bg: 'primary3.600',
+            }}
+            onPress={() => {
+              playSound();
+              setTimeout(() => {
+                if (page < 3) {
+                  setPage(page + 1);
+                }  
+              }, 200);
+            }}
+          />
+        ) : (
+          <Box w="12"></Box>
+        )}
       </HStack>
     </Flex>
   );
