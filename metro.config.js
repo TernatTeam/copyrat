aa47bda230e1f8e7062d6c34efd90574d08c3eba
Cosmin Carsote <cosmin.c@adoreme.com>
2023-03-12 23:32:49 +0200

    ceva

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  resolver: {
-    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs'],
-    assetExts: ['png', 'jpg', 'ttf'],
-  },
-};
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts.push('js', 'jsx', 'json', 'ts', 'tsx', 'cjs');
+
+config.resolver.assetExts.push('png', 'jpg', 'ttf');
+
+module.exports = config;
